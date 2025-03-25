3b6646984715a05ca2b2869f369da9934801cdec
Nithinkumar18 <92311385+Nithinkumar18@users.noreply.github.com>
2025-03-25 15:24:18 +0530

    Update controlRoutes.js

--- backend/src/routes/controlRoutes.js
+++ backend/src/routes/controlRoutes.js
@@ -3,7 +3,6 @@ const controlController = require('../controller/controlController');
 const router = express.Router();
 
 router.post('/acquire',controlController.acquireControl);
-// router.post('/release',controlController.releaseControl);
 router.get('/status', controlController.getControlStatus);  
 
 
